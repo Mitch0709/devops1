d443179d786a82a94a838d00c480620d94b5944c
Mitch <mitch@LAPTOP-MITCH>
2024-05-27 11:54:43 +0200

    .

---
+++ app/PersonClass.py
@@ -0,0 +1,4 @@
+class Person:
+  def __init__(self, name, age): #waarden toe wijzen aan objecteigenschappen
+    self.name = name #self = verwijzing naar huidige instantie + krijgt toegang.
+    self.age = age
